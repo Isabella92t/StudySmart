32d4b0cf2687e2903d1f74e8b22cd86f58c4e538
Isabella <tolkenisabella@gmail.com>
2026-01-13 19:24:56 +0100

    Initial Commit

---
+++ IsabellasLaxApp/IsabellasLaxAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IsabellasLaxAppApp.swift
+//  IsabellasLaxApp
+//
+//  Created by Isabella Heidari on 2026-01-13.
+//
+
+import SwiftUI
+
+@main
+struct IsabellasLaxAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
